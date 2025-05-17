c0d1b3c67e7e44180fabd01bf925aca88d377367
nholderTDM <natholder01@gmail.com>
2025-05-17 00:31:02 -0400

    Update API_URL to use production endpoint

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://areti-crm-api.herokuapp.com/api';
 
 export default API_URL;
